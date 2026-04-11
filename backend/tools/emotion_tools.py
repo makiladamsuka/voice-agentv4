8a1bcddbfc214f6d3ad19cf858c1a9974d4bfb50
makiladamsuka <makiladamsuka09@gmail.com>
2026-04-11 19:50:03 +0530

    Fix async bug in EmotionTools

--- backend/tools/emotion_tools.py
+++ backend/tools/emotion_tools.py
@@ -5,7 +5,7 @@ import logging
 
 class EmotionTools:
     @function_tool(description="Change the physical robot's eye emotion. Use this to express how you feel when speaking.")
-    def set_emotion(self, emotion_name: str, ctx: RunContext):
+    async def set_emotion(self, emotion_name: str, ctx: RunContext):
         """
         Set the physical emotion state of the robot. 
         Valid emotions include: 'happy', 'sad', 'angry', 'surprised', 'suspicious', 'sleepy', 'bored', 'thinking', 'curious_intense', 'attentive', 'amused'.
